f9abe7a1e0687d4e497f0f1a41ab8d5df5055527
Jonathan Mendoza <jonatmendoza@gmail.com>
2024-04-25 23:36:53 -0700

    hero and navbar, responsive design

---
+++ app/components/NavLink.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export interface NavLinkProps {
+  href: string;
+  title: string;
+}
+
+const NavLink = ({ href, title }: NavLinkProps) => {
+  return (
+    <Link href={href} className='block py-2 pl-3 pr-4 text-[#ADB7BE]'>
+      {title}
+    </Link>
+  );
+};
+
+export default NavLink;
